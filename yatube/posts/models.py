7aa8b2cd6945b3c9e6d030156b52d721030e98ef
Nikita Klimoyts <everygreen22@yandex.ru>
2022-05-20 14:56:27 +0300

    Подключена аутентификация

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -30,3 +30,6 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text 
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('posts.urls', namespace='posts')),
-    path('admin/', admin.site.urls)]
+    path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('', include('posts.urls', namespace='posts'))
+]
