28e12fd566e309e07ff81c3e18760db740540d86
Nikita Klimoyts <everygreen22@yandex.ru>
2022-05-21 16:51:17 +0300

    Добавлена форма регистрации

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts'))
 ]
