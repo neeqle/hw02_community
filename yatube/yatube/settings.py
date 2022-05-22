8dfd394b77998b6630f13e4cb932d513e36d7232
Nikita Klimoyts <everygreen22@yandex.ru>
2022-05-22 16:49:23 +0300

    Добавлены изменение и восставновление пароля

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -124,3 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'posts:index'
+LOGOUT_REDIRECT_URL = 'posts:index'
