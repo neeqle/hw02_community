158d9da98fa4cd56fbfc50629b960e8c9d1ef60e
Nikita Klimoyts <everygreen22@yandex.ru>
2022-05-28 14:55:42 +0300

    Теперь дружит с Paginator

---
+++ yatube/core/context_processors/year.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+def year(request):
+    """Добавляет переменную с текущим годом."""
+    return {
+        'year' : datetime.now().year
+    }
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,5 @@
 
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
@@ -6,11 +7,14 @@ POSTS_COUNT = 10
 
 
 def index(request):
-    posts = Post.objects.all()[:POSTS_COUNT]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request, slug):
